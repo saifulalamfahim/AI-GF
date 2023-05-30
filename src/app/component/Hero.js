7d79f4b62e43555e19c79be05072bc5ca4486a18
saifulalamfahime <you@example.com>
2023-05-30 23:33:27 +0600

    testing

---
+++ src/app/component/Hero.js
@@ -0,0 +1,8 @@
+export default function Hero() {
+    return (
+      <main className="">
+        <h1>ami kaj kortasi</h1>
+      </main>
+    )
+  }
+  
---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+// import Image from 'next/image'
+
+import Hero from "./component/hero";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>ami hoilam home</h1>
+      <Hero/>
+    </main>
+  )
+}
